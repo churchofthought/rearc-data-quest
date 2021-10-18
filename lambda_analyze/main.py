44f7d2104ec2dadaee76394a7deb9279c6cfa0da
xibxor <admin@scrollto.com>
2021-10-18 02:49:47 -0400

    all

---
+++ lambda_analyze/main.py
@@ -0,0 +1,7 @@
+
+def lambda_handler(event, context):
+    logger.info('## ENVIRONMENT VARIABLES\r' + jsonpickle.encode(dict(**os.environ)))
+    logger.info('## EVENT\r' + jsonpickle.encode(event))
+    logger.info('## CONTEXT\r' + jsonpickle.encode(context))
+    response = client.get_account_settings()
+    return response['AccountUsage']
